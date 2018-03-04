f1e8e2214124156e49f60c9be8d031813e6a3652
John Stuart <31050027+johnstuart670@users.noreply.github.com>
2018-03-04 13:30:26 -0700

    table rows in place, disabled modal to make it easier to work with on the front end

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,55 @@
+// div id variables for later
 
-  var config = {
-    apiKey: "AIzaSyDxe6ILAFwB7T4vLpS5JPj9-wDpu2OiQtM",
-    authDomain: "hw7-trains-91c4c.firebaseapp.com",
-    databaseURL: "https://hw7-trains-91c4c.firebaseio.com",
-    projectId: "hw7-trains-91c4c",
-    storageBucket: "hw7-trains-91c4c.appspot.com",
-    messagingSenderId: "1080446589226"
-  };
-  firebase.initializeApp(config);
+var header = $("header");
+var schedule = $("#schedule");
+var inputArea = $("#inputArea");
+var footer = $("footer");
 
-	
+// Create the userName variable for later
+var userName = "Probably Gavin, he always forgets to enter his username";
+
+//  firebase info
+var config = {
+	apiKey: "AIzaSyDxe6ILAFwB7T4vLpS5JPj9-wDpu2OiQtM",
+	authDomain: "hw7-trains-91c4c.firebaseapp.com",
+	databaseURL: "https://hw7-trains-91c4c.firebaseio.com",
+	projectId: "hw7-trains-91c4c",
+	storageBucket: "hw7-trains-91c4c.appspot.com",
+	messagingSenderId: "1080446589226"
+};
+firebase.initializeApp(config);
+
+// This somehow stops the enter key from being used to submit forms
+
+// initial function 
+// function initializePage(){
+// // hide the divs on the page
+// header.hide();
+// schedule.hide();
+// inputArea.hide();
+// footer.hide();
+// // show the modal
+// $("#loginModal").modal("show");
+// }
+
 // function for the page
-	$(document).ready(function(){
-
-$("#loginModal").modal("show");
+$(document).ready(function () {
+	// wind it up
+// initializePage();
+	// On click of the submit button on the Modal
+	$("#submitBtn").click(function () {
+		event.preventDefault();
+		// update userName to the input from the user
+		userName = $("#userName").val()
+		console.log(userName);
+	})
+	// when the Modal is dismissed
+	$('#loginModal').on('hidden.bs.modal', function () {
+		// show the elements on the page
+		header.show();
+		schedule.show();
+		inputArea.show();
+		footer.show();
 	});
+
+});
