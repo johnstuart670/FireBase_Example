d37850b3ff10bb6d8c6dd1bf3d70074e7fd5bf53
John Stuart <31050027+johnstuart670@users.noreply.github.com>
2018-03-04 10:47:29 -0700

    modal popup now working, can now build rest of the website

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+
+  var config = {
+    apiKey: "AIzaSyDxe6ILAFwB7T4vLpS5JPj9-wDpu2OiQtM",
+    authDomain: "hw7-trains-91c4c.firebaseapp.com",
+    databaseURL: "https://hw7-trains-91c4c.firebaseio.com",
+    projectId: "hw7-trains-91c4c",
+    storageBucket: "hw7-trains-91c4c.appspot.com",
+    messagingSenderId: "1080446589226"
+  };
+  firebase.initializeApp(config);
+
+	
+// function for the page
+	$(document).ready(function(){
+
+$("#loginModal").modal("show");
+	});
